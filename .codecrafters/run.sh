5eacbfaa8482f519b2ceac54b8cce500ce6a7c0b
Ramcharan-30 <ramcharankodisa.24@kgpian.iitkgp.ac.in>
2026-06-20 18:12:28 +0530

    codecrafters submit [skip ci]

--- .codecrafters/run.sh
+++ .codecrafters/run.sh
@@ -1,19 +1,14 @@
 #!/bin/sh
-#
-# This script is used to run your program on CodeCrafters
-#
-# This runs after .codecrafters/compile.sh
-#
-# Learn more: https://codecrafters.io/program-interface
 
-set -e # Exit on failure
+set -e
 
-exec uv run --quiet -m app.main "$@" # anti-windows-bug
+SCRIPT_DIR="$(dirname "$0")"
 
-
-
-
-
+PYTHONSAFEPATH=1 PYTHONPATH="$SCRIPT_DIR" exec uv run \
+  --project "$SCRIPT_DIR" \
+  --quiet \
+  -m app.main \
+  "$@"
 
 
 
